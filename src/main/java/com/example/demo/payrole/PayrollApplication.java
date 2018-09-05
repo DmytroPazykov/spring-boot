8391b69d1af18b82b6f36db813268b7f54752d2e
dmytro pazykov <c_dpazykov@godaddy.com>
2018-09-05 15:45:04 +0300

    fix test, still not working from console

--- src/main/java/com/example/demo/payrole/PayrollApplication.java
+++ src/main/java/com/example/demo/payrole/PayrollApplication.java
@@ -1,3 +1,5 @@
+package com.example.demo.payrole;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/test/java/com/example/demo/payrole/test/PayrollApplicationTests.java
+++ src/test/java/com/example/demo/payrole/test/PayrollApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo.payrole.test;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import lombok.extern.slf4j.Slf4j;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Slf4j
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PayrollApplicationTests {
@@ -17,9 +21,16 @@ public class PayrollApplicationTests {
     private TestRestTemplate restTemplate;
 
     @Test
+    @Tag("demo")
     public void contextLoads() {
-        String message = this.restTemplate.getForObject("/employees/1", String.class);
-        assertEquals("JUnit 5 and Spring Boot Example.", message);
+        log.info("Starting test");
+        log.info("Starting test");
+        String message = restTemplate
+            .getForObject("/employees/1", String.class);
+
+        log.info("Doing Assert");
+
+        assertEquals("{\"id\":1,\"name\":\"Bilbo Baggins\",\"role\":\"burglar\"}", message);
     }
 
 }
